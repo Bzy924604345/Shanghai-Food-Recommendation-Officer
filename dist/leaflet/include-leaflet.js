d0ecbb8cfaa4516948dd9c9e03cc1d8d33d6dd92
Bzy924604345 <924604345@qq.com>
2024-01-05 17:04:45 +0800

    1.0

--- dist/leaflet/include-leaflet.js
+++ dist/leaflet/include-leaflet.js
@@ -1,6 +1,10 @@
+/* Copyright© 2000 - 2023 SuperMap Software Co.Ltd. All rights reserved.
+ * This program are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at http://www.apache.org/licenses/LICENSE-2.0.html.*/
 (function () {
-    var r = new RegExp("(^|(.*?\\/))(include-leaflet\.js)(\\?|$)"),
-        s = document.getElementsByTagName('script'), targetScript;
+    var r = new RegExp('(^|(.*?\\/))(include-leaflet.js)(\\?|$)'),
+        s = document.getElementsByTagName('script'),
+        targetScript;
     for (var i = 0; i < s.length; i++) {
         var src = s[i].getAttribute('src');
         if (src) {
@@ -34,7 +38,7 @@
     function supportES6() {
         var code = "'use strict'; class Foo {}; class Bar extends Foo {};";
         try {
-            (new Function(code))();
+            new Function(code)();
         } catch (err) {
             return false;
         }
@@ -45,96 +49,136 @@
     }
 
     //加载类库资源文件
-    function load() {
-        var includes = (targetScript.getAttribute('include') || "").split(",");
-        var excludes = (targetScript.getAttribute('exclude') || "").split(",");
+    function load(config) {
+        var libsurl = config.libsurl;
+        var disturl = config.disturl;
+        var includes = (targetScript.getAttribute('include') || '').split(',');
+        var excludes = (targetScript.getAttribute('exclude') || '').split(',');
         // 在线
         if (!inArray(excludes, 'leaflet')) {
-            inputCSS("https://cdn.bootcss.com/leaflet/1.5.1/leaflet.css");
-            inputScript("https://cdn.bootcss.com/leaflet/1.5.1/leaflet.js");
+          inputCSS(libsurl + '/leaflet/1.9.4/leaflet.css');
+          inputScript(libsurl + '/leaflet/1.9.4/leaflet.js');
+        }
+        if (inArray(includes, 'mapbox-gl-enhance')) {
+            inputCSS(libsurl + '/mapbox-gl-js-enhance/1.12.1-3/mapbox-gl-enhance.css');
+            inputScript(libsurl + '/mapbox-gl-js-enhance/1.12.1-3/mapbox-gl-enhance.js');
         }
         if (inArray(includes, 'leaflet.heat')) {
-          inputScript("https://cdn.bootcss.com/leaflet.heat/0.2.0/leaflet-heat.js");
+            inputScript(libsurl + '/leaflet/plugins/leaflet.heat/leaflet-heat.js');
         }
         if (inArray(includes, 'leaflet.markercluster')) {
-            inputCSS("https://cdn.bootcss.com/leaflet.markercluster/1.4.1/MarkerCluster.Default.css");
-            inputCSS("https://cdn.bootcss.com/leaflet.markercluster/1.4.1/MarkerCluster.css");
-            inputScript("https://cdn.bootcss.com/leaflet.markercluster/1.4.1/leaflet.markercluster.js");
+            inputCSS(libsurl + '/leaflet/plugins/leaflet.markercluster/1.5.3/MarkerCluster.Default.css');
+            inputCSS(libsurl + '/leaflet/plugins/leaflet.markercluster/1.5.3/MarkerCluster.css');
+            inputScript(libsurl + '/leaflet/plugins/leaflet.markercluster/1.5.3/leaflet.markercluster.js');
         }
         if (inArray(includes, 'leaflet.draw')) {
-            inputCSS("https://cdn.bootcss.com/leaflet.draw/1.0.4/leaflet.draw.css");
-            inputScript("https://cdn.bootcss.com/leaflet.draw/1.0.4/leaflet.draw.js");
+            inputCSS(libsurl + '/leaflet/plugins/leaflet.draw/1.0.4/leaflet.draw.css');
+            inputScript(libsurl + '/leaflet/plugins/leaflet.draw/1.0.4/leaflet.draw.js');
         }
-        if (inArray(includes, 'leaflet.pm')) {
-            inputCSS("https://cdn.bootcss.com/leaflet.pm/2.2.0/leaflet.pm.css");
-            inputScript("https://cdn.bootcss.com/leaflet.pm/2.2.0/leaflet.pm.min.js");
+        if (inArray(includes, 'leaflet-geoman')) {
+          inputCSS(libsurl + '/leaflet/plugins/leaflet-geoman/2.14.2/leaflet-geoman.css');
+          inputScript(libsurl + '/leaflet/plugins/leaflet-geoman/2.14.2/leaflet-geoman.min.js');
+
         }
         if (inArray(includes, 'leaflet.miniMap')) {
-            inputCSS("https://cdn.bootcss.com/leaflet-minimap/3.6.1/Control.MiniMap.min.css");
-            inputScript("https://cdn.bootcss.com/leaflet-minimap/3.6.1/Control.MiniMap.min.js");
+            inputCSS(libsurl + '/leaflet/plugins/leaflet-miniMap/3.6.1/dist/Control.MiniMap.min.css');
+            inputScript(libsurl + '/leaflet/plugins/leaflet-miniMap/3.6.1/dist/Control.MiniMap.min.js');
+        }
+        if (inArray(includes, 'g6')) {
+          inputScript(libsurl + '/antv/g6/4.3.2/g6.min.js');
         }
         if (inArray(includes, 'mapv')) {
-            inputScript("http://mapv.baidu.com/build/mapv.min.js");
+            inputScript(libsurl + '/mapv/2.0.62/mapv.min.js');
         }
         if (inArray(includes, 'turf')) {
-            inputScript("https://cdn.bootcss.com/Turf.js/5.1.6/turf.min.js");
+            inputScript(libsurl + '/turf/6.5.0/turf.min.js');
         }
         if (inArray(includes, 'echarts')) {
-          inputScript("https://cdn.bootcss.com/echarts/4.3.0-rc.2/echarts.min.js");
+          inputScript(libsurl + '/echarts/5.4.3/echarts.min.js');
         }
         if (inArray(includes, 'elasticsearch')) {
-            inputScript("https://cdn.bootcss.com/elasticsearch/16.3.0/elasticsearch.js");
+            inputScript(libsurl + '/elasticsearch/16.7.3/elasticsearch.min.js');
         }
         if (inArray(includes, 'xlsx')) {
-            inputScript("https://cdn.bootcss.com/xlsx/0.15.1/xlsx.core.min.js");
+            inputScript(libsurl + '/xlsx/0.19.3/xlsx.core.min.js');
         }
         // 本地
         if (inArray(includes, 'leaflet.sidebyside')) {
-            inputScript("http://iclient.supermap.io/libs/leaflet/plugins/leaflet-side-by-side/leaflet-side-by-side.min.js");
+            inputScript(libsurl + '/leaflet/plugins/leaflet-side-by-side/2.0.1/leaflet-side-by-side.min.js');
         }
         if (inArray(includes, 'd3')) {
-          inputScript("http://iclient.supermap.io/web/libs/d3/5.12.0/d3.js");
+            inputScript(libsurl + '/d3/7.8.2/d3.min.js');
         }
         if (inArray(includes, 'd3-hexbin')) {
-          inputScript("https://d3js.org/d3-hexbin.v0.2.min.js");
+            inputScript(libsurl + '/d3-hexbin/0.2.2/d3-hexbin.v0.2.min.js');
         }
         if (inArray(includes, 'd3Layer')) {
-            inputScript("http://iclient.supermap.io/libs/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.js");
+            inputScript(libsurl + '/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.js');
         }
         if (inArray(includes, 'osmbuildings')) {
-          inputScript("http://iclient.supermap.io/web/libs/osmbuildings/OSMBuildings-Leaflet.js");
+            inputScript(libsurl + '/osmbuildings/OSMBuildings-Leaflet.js');
         }
         if (inArray(includes, 'leaflet-icon-pulse')) {
-          inputCSS("http://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css");
-          inputScript("http://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js");
+            inputCSS(libsurl + '/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css');
+            inputScript(libsurl + '/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js');
         }
         if (inArray(includes, 'deck')) {
-          inputScript("http://iclient.supermap.io/web/libs/deck.gl/5.1.3/deck.gl.min.js");
+            inputScript(libsurl + '/deck.gl/5.1.3/deck.gl.min.js');
+        }
+        if (inArray(includes, 'pixi')) {
+            inputScript(libsurl + '/pixi/4.8.7/pixi.min.js');
+            inputScript(libsurl + '/leaflet/plugins/Leaflet.PixiOverlay/1.9.4/L.PixiOverlay.min.js');
+            inputScript(libsurl + '/leaflet/plugins/Leaflet.PixiOverlay/MarkerContainer.js');
+            inputScript(libsurl + '/bezier-easing/2.1.0/bezier-easing.js');
+        }
+        if (inArray(includes, 'tensorflow')) {
+            inputScript(libsurl + '/tensorflow/3.9.0/tf.min.js');
         }
 
         // iclient
         if (!inArray(excludes, 'iclient-leaflet')) {
-          if (supportES6()) {
-              inputScript("dist/leaflet/iclient-leaflet-es6.min.js");
-          } else {
-              inputScript("dist/leaflet/iclient-leaflet.min.js");
-          }
+            if (supportES6()) {
+                inputScript(disturl + '/leaflet/iclient-leaflet-es6.min.js');
+            } else {
+                inputScript(disturl + '/leaflet/iclient-leaflet.min.js');
+            }
         }
         if (inArray(includes, 'iclient-leaflet-css')) {
-            inputCSS("dist/leaflet/iclient-leaflet.min.css");
+            inputCSS(disturl + '/leaflet/iclient-leaflet.min.css');
         }
         if (inArray(includes, 'iclient-plot-leaflet')) {
-            inputCSS("http://iclient.supermap.io/web/libs/plotting/leaflet/10.0.0/iclient-plot-leaflet.css");
+            inputCSS(libsurl + '/plotting/leaflet/11.1.0/iclient-plot-leaflet.css');
             if (supportES6()) {
-                inputScript("http://iclient.supermap.io/web/libs/plotting/leaflet/10.0.0/iclient-plot-leaflet-es6.min.js");
+                inputScript(libsurl + '/plotting/leaflet/11.1.1/iclient-plot-leaflet-es6.min.js');
             } else {
-                inputScript("http://iclient.supermap.io/web/libs/plotting/leaflet/10.0.0/iclient-plot-leaflet.min.js");
+                inputScript(libsurl + '/plotting/leaflet/11.1.1/iclient-plot-leaflet.min.js');
             }
+        }
+        if (inArray(includes, 'ant-design-vue')) {
+            inputCSS(libsurl + '/ant-design-vue/1.7.8/antd.min.css');
+            inputScript(libsurl + '/ant-design-vue/1.7.8/antd.min.js');
+        }
+        if (inArray(includes, 'echarts-vue')) {
+            inputScript(libsurl + '/echarts/4.9.0/echarts.min.js');
+            inputScript(libsurl + '/vue-echarts/4.1.0/vue-echarts.min.js');
+            inputScript(libsurl + '/echarts-liquidfill/2.0.6/echarts-liquidfill.min.js');
+            inputScript(libsurl + '/echartsLayer/EchartsLayer.min.js');
+        }
+        if (inArray(includes, 'iclient-leaflet-vue')) {
+            inputCSS(disturl + '/leaflet/iclient-leaflet-vue.css');
+            inputScript(disturl + '/leaflet/iclient-leaflet-vue.min.js');
+        }
+        if (inArray(includes, 'leaflet-mapbox-gl')) {
+            inputScript(libsurl + '/leaflet-mapbox-gl/0.0.15/leaflet-mapbox-gl.min.js');
         }
     }
 
-
-    load();
+    load({
+        libsurl: '../../libs',
+        disturl: '../../dist'
+    });
     window.isLocal = false;
-    window.server = document.location.toString().match(/file:\/\//) ? "http://localhost:8090" : document.location.protocol + "//" + document.location.host;
+    window.server = document.location.toString().match(/file:\/\//)
+        ? 'http://localhost:8090'
+        : document.location.protocol + '//' + document.location.host;
 })();
--- js/main.js
+++ js/main.js
@@ -1,80 +1,254 @@
-//服务地址
-var map, url = "http://localhost:8091/iserver/services/map-Campus/rest/maps/Campus";
+document.addEventListener('DOMContentLoaded', function () {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYmFvMTgyIiwiYSI6ImNscWY5MmtsODB1Z3kybG83Z29ieXB5emsifQ.gs_CFBWs4sFEiwYq9NeXrg';
 
-var url_china = "http://localhost:8091/iserver/services/map-china400/rest/maps/China_4326";
-function onPageLoad() {
-  // 创建地图窗口
-  // 南师大
-  // map = L.map('map', {
-  //   crs: L.CRS.EPSG4326,
-  //   center: [32.1101, 118.902],
-  //   // minZoom: 14,
-  //   maxZoom: 18,
-  //   zoom: 14
-  // });
-
-  // 上海市
-  map = L.map('map', {
-    crs: L.CRS.EPSG4326,
-    center: [31.11, 121.45],
-    // minZoom: 14,
-    maxZoom: 18,
-    zoom: 9
-  });
-  // 添加地图
-  new L.supermap.TiledMapLayer(url_china).addTo(map);
-
-  new L.supermap.TiledMapLayer(url).addTo(map);
-
-  // 比例尺
-  L.control.scale().addTo(map);
-
-  // 绘制控件
-  var editableLayers = new L.FeatureGroup();
-  map.addLayer(editableLayers);
-  var options = {
-    position: 'topleft',
-    draw: {
-      polyline: {},
-      polygon: {},
-      circle: {},
-      rectangle: {},
-      marker: {},
-      remove: {}
-    },
-    edit: {
-      featureGroup: editableLayers,
-      remove: true
-    }
-  };
-  var drawControl = new L.Control.Draw(options);
-  map.addControl(drawControl);
-  handleMapEvent(drawControl._container, map);
-  map.on(L.Draw.Event.CREATED, function (e) {
-    var type = e.layerType,
-      layer = e.layer;
-    if (type === 'marker') {
-      layer.bindPopup('A popup!');
-    }
-    editableLayers.addLayer(layer);
+  const map = new mapboxgl.Map({
+    container: 'map-container',
+    style: 'mapbox://styles/bao182/clgg44whl002q01pkai118y1f',
+    center: [121.67, 31.26],
+    zoom: 10
   });
 
-  function handleMapEvent(div, map) {
-    if (!div || !map) {
-      return;
-    }
-    div.addEventListener('mouseover', function () {
-      map.scrollWheelZoom.disable();
-      map.doubleClickZoom.disable();
-    });
-    div.addEventListener('mouseout', function () {
-      map.scrollWheelZoom.enable();
-      map.doubleClickZoom.enable();
-    });
-  }
-  // 鹰眼图
-  var osm = new L.supermap.TiledMapLayer(url_china, { minZoom: 0, maxZoom: 13, noWrap: true });
+  map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+});
 
-  L.control.minimap(osm, { mapOptions: { logoControl: false }, toggleDisplay: true }).addTo(map);
+function rotateCircle210() {
+  var circle = document.querySelector('.circle');
+  var text_div01 = document.querySelector('.half01-text');
+  var text_div02 = document.querySelector('.half02-text');
+  circle.classList.remove('rotate-390');
+  text_div01.classList.toggle('div-rotate-150')
+  text_div02.classList.toggle('div-rotate-150')
+  text_div02.classList.remove('text-selected')
+  text_div01.classList.toggle('text-selected')
+  circle.classList.toggle('rotate-210');
 }
 
+var filter_count = 0;
+function rotateCircle330() {
+  filter_count++;
+  // 控制显示
+  var circle = document.querySelector('.circle');
+  var text_div01 = document.querySelector('.half01-text');
+  var text_div02 = document.querySelector('.half02-text');
+  circle.classList.remove('rotate-210');
+  text_div01.classList.remove('div-rotate-150')
+  text_div02.classList.remove('div-rotate-150')
+  text_div01.classList.remove('text-selected')
+  text_div02.classList.toggle('text-selected')
+  circle.classList.toggle('rotate-390');
+
+  if (filter_count % 2 === 1) {
+    // 显示图层
+  }
+  else if (filter_count % 2 === 0) {
+    // 隐藏图层
+  }
+
+  if (filter_count === 1) {
+    // 监听筛选器
+    var dropdowns = document.querySelectorAll('.filterForm select');
+
+    dropdowns.forEach(function (dropdown) {
+      dropdown.addEventListener('change', function () {
+        console.log(dropdown.id + ' 的值变为：', dropdown.value);
+        // 这里根据根据修改的筛选条件再显示图层
+      });
+    });
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var circle = document.querySelector('.circle');
+  var searchContainer = document.querySelector('.searchContainer');
+  var filterContainer = document.querySelector('.filterContainer');
+  var search_results_container = document.querySelector('.search_results_container');
+  var search_btn = document.querySelector('.search_btn')
+  // 监听当前点击状态
+  circle.addEventListener('click', function (event) {
+    var isSearch = circle.classList.contains('rotate-210');
+    var isFilter = circle.classList.contains('rotate-390');
+    // 当前为搜索状态
+    if (isSearch) {
+      // 取消筛选组件
+      filterContainer.style.animation = 'slideOut 0.5s ease';
+      setTimeout(function () {
+        filterContainer.style.display = 'none';
+      }, 500);
+      // 展开搜索栏
+      searchContainer.style.display = 'block';
+      searchContainer.style.animation = 'slideIn 0.5s ease';
+      // 点击按钮出现搜索弹窗
+      search_btn.addEventListener('click', function (event) {
+        search_results_container.style.display = 'block';
+        search_results_container.style.animation = 'resultsIn 0.5s ease';
+      })
+    }
+    else if (isFilter) {
+      searchContainer.style.animation = 'slideOut 0.5s ease';
+      setTimeout(function () {
+        searchContainer.style.display = 'none';
+      }, 500);
+      filterContainer.style.display = 'block';
+      filterContainer.style.animation = 'slideIn 0.5s ease';
+    }
+    else {
+      searchContainer.style.animation = 'slideOut 0.5s ease';
+      filterContainer.style.animation = 'slideOut 0.5s ease';
+      setTimeout(function () {
+        searchContainer.style.display = 'none';
+        filterContainer.style.display = 'none';
+      }, 500);
+      // 搜索弹窗消失
+      search_results_container.style.animation = 'resultsOut 0.5s ease';
+      search_results_container.style.display = 'none';
+    }
+  });
+});
+
+// AIsearch 点击显示与隐藏
+document.addEventListener('DOMContentLoaded', function () {
+  var AIsearch_btn_container = document.querySelector('.AIsearch_btn_container')
+  var AIsearch_btn = document.querySelector('.AIsearch_btn_container .AIsearch_btn')
+  var arrow_wrapper = document.querySelector('.AIsearch_btn_container .AIsearch_btn .arrow-wrapper')
+  var answer_content = document.querySelector('.answer_content')
+  var ask_input = document.querySelector('.ask_input')
+  let clickCount = 0;
+
+  AIsearch_btn.addEventListener('click', function () {
+    clickCount++;
+
+    if (clickCount % 2 === 1) {
+      // 奇数次点击 -> 显示
+      AIsearch_btn_container.classList.add('AIsearch_window_show');
+      AIsearch_btn_container.classList.remove('AIsearch_window_hidden');
+      arrow_wrapper.style.transform = 'rotate(0deg)';
+
+      answer_content.style.display = 'block'
+      answer_content.style.animation = 'ease-out answer_content_In 0.5s 1s forwards'
+      ask_input.style.display = 'flex'
+    } else {
+      // 偶数次点击 -> 隐藏
+      AIsearch_btn_container.classList.add('AIsearch_window_hidden');
+      AIsearch_btn_container.classList.remove('AIsearch_window_show');
+      arrow_wrapper.style.transform = 'rotate(180deg)';
+
+      answer_content.style.display = 'none'
+      ask_input.style.display = 'none'
+    }
+  })
+
+  // 切换麦克风图片
+  var voice_input = document.querySelector('.voice_input');
+  var micImage = document.querySelector('.voice_input img');
+
+  voice_input.addEventListener('mousedown', function () {
+    micImage.src = '../images/麦克风-激活.png'; // 切换为新的图片路径
+  });
+
+  voice_input.addEventListener('mouseup', function () {
+    micImage.src = '../images/麦克风.png'; // 恢复为原始图片路径
+  });
+})
+
+// 搜索与显示
+function searchRestaurant() {
+  var searchParam = document.querySelector('.searchContainer .input').value;
+  var search_results_container = document.querySelector('.search_results_container');
+  search_results_container.innerHTML = '';
+
+  // 遍历饭店点集查询
+  Points.forEach(point => {
+    var target_name = point.getName()
+    // 使用 includes 方法进行模糊查询
+    if (target_name.includes(searchParam)) {
+      var resultContainer = document.createElement('div');
+      resultContainer.classList.add('result_items');
+      // 判断评分
+      if (point.star < 3) {
+        var star_pic = 'stat_3'
+      }
+      else if (point.star > 3 && point.star < 4) {
+        var star_pic = 'stat_4'
+      }
+      else if (point.star > 5) {
+        var star_pic = 'stat_5'
+      }
+
+      var content = `
+          <div class="row1">
+            <div class="restaurant_name">${target_name}</div>
+            <div class="cuisine">${point.cuisineType}</div>
+          </div>
+          <div class="row2">
+            <img class="stars" src="./images/${star_pic}.png">
+            <span class="score">${point.score}</span>
+            <div class="per">${point.per}</div>
+          </div>
+          <div class="row3">
+            <div class="address">${point.address}</div>
+          </div>
+          <div class="horizontal-line"></div>
+      `;
+
+      // 将内容添加到 resultContainer 中
+      resultContainer.innerHTML = content;
+
+      // 将 resultContainer 添加到 search_results_container 中
+      search_results_container.appendChild(resultContainer);
+
+      // 点击后聚焦位置并弹窗，两种思路：搜索后为每一个结果添加监听器（最省事）；读取点击div信息，根据id或者name找
+      resultContainer.addEventListener('click', function () {
+        map.setZoomAndCenter(14, labelMarker.getPosition(), false, 600);
+        labelMarker.setIcon({
+          size: [40, 40]
+        })
+        var position = labelMarker.getPosition()
+
+        var contentMarker = new AMap.Marker({
+          anchor: 'bottom-center',
+          offset: [0, 160],
+        });
+
+        if (position) {
+          contentMarker.setContent(
+            '<div style="font-family: Arial, sans-serif; background-color: #ffffff; border: 1px solid #cccccc; padding: 10px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">' +
+            '<div style="font-size: 14px; font-weight: bold; margin-bottom: 5px;">经停地铁：' + '<br>' + labelMarker.getExtData().address + '</div>' +
+            '<div style="color: #007BFF; margin-bottom: 5px;">市辖区：' + labelMarker.getExtData().adname + '</div>' +
+            '<div>经纬度：' + position + '</div>' +
+            '</div>')
+
+          contentMarker.setPosition(position);
+          map.add(contentMarker);
+
+          // 取消选中
+          map.on('click', function () {
+            labelMarker.setIcon({
+              size: [24, 24]
+            })
+
+            map.remove(contentMarker);
+
+          })
+        }
+      })
+
+    }
+  })
+}
+
+function clearInput() {
+  document.querySelector(".pbuffer input").value = "";
+}
+
+function showbuffersearch() {
+  var plnglat = document.querySelector('.buffersearch_container .plnglat')
+  var pbuffer = document.querySelector('.pbuffer')
+  plnglat.style.display = 'block'
+  plnglat.style.animation = 'ease-out plnglatIn 0.6s 0s forwards'
+
+  setTimeout(function () {
+    pbuffer.style.display = 'flex'
+    pbuffer.style.animation = 'ease-out bufferIn 0.6s 0s forwards';
+  }, 300);
+}
